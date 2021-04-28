540ffb6b5cc95a34317074d8f4d28ba64f05b5fb
Natanael Lima <taelima1997@gmail.com>
2021-04-27 23:26:37 -0300

    add repositories categorie

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,11 @@
+import { Specification } from "../entities/Specifications";
+
+interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+}
+interface ISpecificationsRepository {
+  create({ name, description }: ICreateSpecificationDTO): void;
+  findByName(name: string): Specification;
+}
+export { ISpecificationsRepository, ICreateSpecificationDTO };
