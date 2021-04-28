efe9748bc9f51a25cd03fab815a1e3d051d1fa45
Natanael Lima <taelima1997@gmail.com>
2021-04-27 23:27:20 -0300

    add use case list categories

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,11 @@
+import { Category } from "../../entities/Category";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+    return categories;
+  }
+}
+export { ListCategoriesUseCase };
