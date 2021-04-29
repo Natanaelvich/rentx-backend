23ccdc1b6ef55a6e96bd4890925adc397c673863
Natanael Lima <taelima1997@gmail.com>
2021-04-28 21:45:57 -0300

    add initial routes categories

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,10 @@
 // import appointmentsRouter from '@modules/Appointmensts/infra/http/routes/appointments.routes';
 
 import { Router } from 'express';
+import { categoriesRoutes } from './categories.routes';
 
 const routes = Router();
 
-// routes.use('/sessions', sessionsRouter);
+routes.use('/categories', categoriesRoutes);
 
 export default routes;
