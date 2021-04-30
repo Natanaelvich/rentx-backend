77c2d184b576853402eac25e4f2321bbe94445ed
Natanael Lima <taelima1997@gmail.com>
2021-04-29 23:22:13 -0300

    add category depedency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -23,3 +23,11 @@ import './providers';
 //   'UserTokenRepository',
 //   UserTokenRepository,
 // );
+import { container } from 'tsyringe';
+import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
+import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
+
+container.registerSingleton<ICategoriesRepository>(
+  'CategoriesRepository',
+  CategoriesRepository,
+);
