3f69501254f8d333e2747b83e975d7982ff34226
Natanael Lima <taelima1997@gmail.com>
2021-05-01 11:32:16 -0300

    update CreateCategoryController

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express';
-import { container } from 'tsyringe';
+// import { container } from 'tsyringe';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 class CreateCategoryController {
-  async handle(request: Request, response: Response): Promise<Response> {
-    const { name, description } = request.body;
-    const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
-    await createCategoryUseCase.execute({ name, description });
+  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-    return response.status(201).send();
+  handle(request: Request, response: Response): Response {
+    const { name, description } = request.body;
+    // const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
+    this.createCategoryUseCase.execute({ name, description });
+
+    return response.send();
   }
 }
 
