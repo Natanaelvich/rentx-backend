9c65331d21d70dc09c21c72373de5b8a39fee4fe
Natanael Lima <taelima1997@gmail.com>
2021-05-01 11:33:02 -0300

    update route create

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import multer from 'multer';
 import { Router } from 'express';
 import { listCategoriesController } from '@modules/cars/useCases/listCategories';
-import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
+import { createCategoryController } from '@modules/cars/useCases/createCategory';
 import { importCategoryController } from '@modules/cars/useCases/importCategory';
 // import { createCategoryController } from '../modules/cars/useCases/createCategory';
 // import { importCategoryController } from '../modules/cars/useCases/importCategory';
@@ -15,9 +15,9 @@ const upload = multer({
 //   return createCategoryController.handle(request, response);
 // });
 
-const createCategoryController = new CreateCategoryController();
-
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post('/', (request, response) => {
+  return createCategoryController.handle(request, response);
+});
 
 categoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
