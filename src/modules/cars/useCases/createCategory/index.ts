83e5c7f83ad35fac89c5dd600c957713b521acc3
Natanael Lima <taelima1997@gmail.com>
2021-05-01 11:32:52 -0300

    add object on controller

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,10 +1,13 @@
-// import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-// import { CreateCategoryController } from "./CreateCategoryController";
-// import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+import { CategoriesRepository } from '@modules/cars/repositories/implementations/CategoriesRepository';
+import { CreateCategoryController } from './CreateCategoryController';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
-// export default (): CreateCategoryController => {
-//   const categoriesRepository = new CategoriesRepository();
-//   const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-//   const createCategoryController = new CreateCategoryController(createCategoryUseCase);
-//   return createCategoryController;
-// };
+const categoriesRepository = CategoriesRepository.getInstance();
+
+const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+
+const createCategoryController = new CreateCategoryController(
+  createCategoryUseCase,
+);
+
+export { createCategoryController };
