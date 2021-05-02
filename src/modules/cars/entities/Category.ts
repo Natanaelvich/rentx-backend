1ed6ce61fb1e366e06a4f9695d3c11263a73dfe3
Natanael Lima <taelima1997@gmail.com>
2021-05-02 11:35:19 -0300

    update name file

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,14 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
 @Entity('categories')
 class Category {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn('uuid')
   id?: string;
 
   @Column()
