43c843015cd589225dd8b3212fa9ed37a3b7b9c6
Natanael Lima <taelima1997@gmail.com>
2021-05-02 11:53:56 -0300

    add specificationsRoutes

---
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -0,0 +1,10 @@
+import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
+import { Router } from 'express';
+
+const specificationsRoutes = Router();
+
+const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.post('/', createSpecificationController.handle);
+
+export { specificationsRoutes };
