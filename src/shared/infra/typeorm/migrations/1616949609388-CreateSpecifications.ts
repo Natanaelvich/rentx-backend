d294990e6829b343e91dfc9c93db19851e20bbde
Natanael Lima <taelima1997@gmail.com>
2021-05-02 11:54:18 -0300

    add migrtion specifications

---
+++ src/shared/infra/typeorm/migrations/1616949609388-CreateSpecifications.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateSpecifications1616949609388 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'specifications',
+
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('specifications');
+  }
+}
