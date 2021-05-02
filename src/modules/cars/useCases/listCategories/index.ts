587225b1e888bc8bcd80b381650e77010bbaf614
Natanael Lima <taelima1997@gmail.com>
2021-05-02 11:36:20 -0300

    commit  index usecase list categories

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,11 +1,11 @@
-import { CategoriesRepository } from '@modules/cars/repositories/implementations/CategoriesRepository';
-import { ListCategoriesController } from './ListCategoriesController';
-import { ListCategoriesUseCase } from './ListCategoriesUseCase';
+// import { CategoriesRepository } from '@modules/cars/repositories/implementations/CategoriesRepository';
+// import { ListCategoriesController } from './ListCategoriesController';
+// import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
-const categoriesRepository = null;
-const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
-const listCategoriesController = new ListCategoriesController(
-  listCategoriesUseCase,
-);
+// const categoriesRepository = null;
+// const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+// const listCategoriesController = new ListCategoriesController(
+//   listCategoriesUseCase,
+// );
 
-export { listCategoriesController };
+// export { listCategoriesController };
