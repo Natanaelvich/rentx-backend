0dc23f607aab94c1c7ae86cca606017432b6f87f
Natanael Lima <taelima1997@gmail.com>
2021-05-02 10:21:20 -0300

    update with dependency injection

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,4 +1,6 @@
 // import { inject, injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
+
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IRequest {
@@ -6,8 +8,12 @@ interface IRequest {
   description: string;
 }
 
+@injectable()
 class CreateCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository,
+  ) {}
 
   async execute({ name, description }: IRequest): Promise<void> {
     const categoryExists = await this.categoriesRepository.findByName(name);
