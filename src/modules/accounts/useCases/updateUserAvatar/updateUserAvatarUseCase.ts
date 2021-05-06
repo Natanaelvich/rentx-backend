6d53c133fcc9e0b533ae4a8278bccf96115739cd
Natanael Lima <taelima1997@gmail.com>
2021-05-05 22:52:24 -0300

    Add updateUserAvatarUseCase

---
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found!', 400);
+    }
+
+    user.avatar = avatar_file;
+
+    await this.usersRepository.create(user);
+  }
+}
+
+export { UpdateUserAvatarUseCase };
