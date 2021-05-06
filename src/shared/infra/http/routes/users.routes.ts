6bcdd3b906c932dc72a7f90dfeda9ab0dd902a11
Natanael Lima <taelima1997@gmail.com>
2021-05-05 23:01:42 -0300

    Add ensureAuthenticated middleware

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,6 +4,7 @@ import multer from 'multer';
 import upload from '@config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/createUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/updateUserAvatarController';
+import { ensureAuthenticated } from '@shared/infra/middlewares/ensureAuthenticated';
 
 const uploader = multer(upload.multer);
 
@@ -17,6 +18,7 @@ usersRoutes.post('/', createUserController.handle);
 
 usersRoutes.patch(
   '/avatar',
+  ensureAuthenticated,
   uploader.single('avatar'),
   updateUserAvatarController.handle,
 );
