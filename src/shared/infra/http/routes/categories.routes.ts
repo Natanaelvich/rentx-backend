7678b71ff9b61119be177167983327fd40686c38
Natanael Lima <taelima1997@gmail.com>
2021-05-05 22:59:35 -0300

    update multer config

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,14 +1,14 @@
-import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
-import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
-import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
 import { Router } from 'express';
 import multer from 'multer';
 
+import upload from '@config/upload';
+import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
+import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
+import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
+
 const categoriesRoutes = Router();
 
-const upload = multer({
-  dest: './tmp',
-});
+const uploader = multer(upload.multer);
 
 const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
@@ -20,7 +20,7 @@ categoriesRoutes.get('/', listCategoriesController.handle);
 
 categoriesRoutes.post(
   '/import',
-  upload.single('file'),
+  uploader.single('file'),
   importCategoriesController.handle,
 );
 
