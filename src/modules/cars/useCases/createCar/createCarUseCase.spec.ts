f60e0b081a208cc9c727e58caf3bd6aea2874187
Natanael Lima <taelima1997@gmail.com>
2021-05-08 09:11:36 -0300

    add test create car with license_plate already in use

--- src/modules/cars/useCases/createCar/createCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/createCarUseCase.spec.ts
@@ -1,8 +1,18 @@
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import AppError from '@shared/errors/AppError';
 import { CreateCarUseCase } from './CreateCarUseCase';
 
 let createCarUseCase: CreateCarUseCase;
 let carsRepositoryInMemory: CarsRepositoryInMemory;
+const mockCar = {
+  name: 'Car Name',
+  description: 'Description',
+  daily_rate: 100,
+  license_plate: 'ABC-1234',
+  fine_amount: 60,
+  brand: 'Brand',
+  category_id: 'Category',
+};
 
 describe('Create car use case', () => {
   beforeEach(() => {
@@ -11,14 +21,13 @@ describe('Create car use case', () => {
   });
 
   it('Should be able to create a new car', async () => {
-    await createCarUseCase.execute({
-      name: 'Car Name',
-      description: 'Description',
-      daily_rate: 100,
-      license_plate: 'ABC-1234',
-      fine_amount: 60,
-      brand: 'Brand',
-      category_id: 'Category',
-    });
+    await createCarUseCase.execute(mockCar);
+  });
+
+  it('Should not be able to create with license_plate already in use', async () => {
+    expect(async () => {
+      await createCarUseCase.execute(mockCar);
+      await createCarUseCase.execute(mockCar);
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
