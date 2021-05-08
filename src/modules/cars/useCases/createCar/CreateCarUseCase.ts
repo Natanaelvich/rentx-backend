9f0d0bfb38370828d16c802551071c833be9f801
Natanael Lima <taelima1997@gmail.com>
2021-05-08 09:12:13 -0300

    add verification if car exists with license_plate

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import AppError from '@shared/errors/AppError';
 
 interface IRequest {
   name: string;
@@ -26,6 +27,14 @@ class CreateCarUseCase {
     brand,
     category_id,
   }: IRequest): Promise<void> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate,
+    );
+
+    if (carAlreadyExists) {
+      throw new AppError('Car already exists');
+    }
+
     await this.carsRepository.create({
       name,
       description,
