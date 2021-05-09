4c537c2c65f79f9d4f118a4281d4f7dacf5dd438
Natanael Lima <taelima1997@gmail.com>
2021-05-09 09:34:04 -0300

    add middewares   ensureAuthenticated and ensureAdmin to posts

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,8 @@ import upload from '@config/upload';
 import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
+import { ensureAdmin } from '@shared/infra/middlewares/ensureAdmin';
+import { ensureAuthenticated } from '@shared/infra/middlewares/ensureAuthenticated';
 
 const categoriesRoutes = Router();
 
@@ -14,12 +16,19 @@ const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
 const importCategoriesController = new ImportCategoryController();
 
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle,
+);
 
 categoriesRoutes.get('/', listCategoriesController.handle);
 
 categoriesRoutes.post(
   '/import',
+  ensureAuthenticated,
+  ensureAdmin,
   uploader.single('file'),
   importCategoriesController.handle,
 );
