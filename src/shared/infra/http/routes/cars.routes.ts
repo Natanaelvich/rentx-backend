fc96fb40657104f4ddd75f8700073bd49f9c6d98
Natanael Lima <taelima1997@gmail.com>
2021-05-09 09:26:42 -0300

    add middlewares authentication and admin

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/createCarController';
+import { ensureAuthenticated } from '@shared/infra/middlewares/ensureAuthenticated';
+import { ensureAdmin } from '@shared/infra/middlewares/ensureAdmin';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 
-carsRoutes.post('/', createCarController.handle);
+carsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle,
+);
 
 export { carsRoutes };
