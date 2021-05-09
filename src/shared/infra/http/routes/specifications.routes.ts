5bbfe49040f53c31e6b619b9388978fdc3b8326e
Natanael Lima <taelima1997@gmail.com>
2021-05-09 09:34:55 -0300

    add middeware authentication and isadmin

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,10 +1,17 @@
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
+import { ensureAdmin } from '@shared/infra/middlewares/ensureAdmin';
+import { ensureAuthenticated } from '@shared/infra/middlewares/ensureAuthenticated';
 import { Router } from 'express';
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle,
+);
 
 export { specificationsRoutes };
