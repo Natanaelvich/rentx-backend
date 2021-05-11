df175d0b655374705d964b9d08d2ec990e993dcc
Natanael Lima <taelima1997@gmail.com>
2021-05-10 22:40:07 -0300

    add inject depencencies

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -2,15 +2,18 @@ import { Car } from '@modules/cars/entities/Car';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
 
 interface IRequest {
   car_id: string;
   specifications_ids: string[];
 }
-
+@injectable()
 class CreateCarSpecificationUseCase {
   constructor(
+    @inject('CarsRepository')
     private carsRepository: ICarsRepository,
+    @inject('SpecificationsRepository')
     private specificationRepository: ISpecificationsRepository,
   ) {}
 
