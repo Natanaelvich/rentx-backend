279611e8b37d9ee6aae7b39ceba7fa820e222fa8
Natanael Lima <taelima1997@gmail.com>
2021-05-10 22:16:31 -0300

    add createCarSpecificationUseCase use case

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -0,0 +1,20 @@
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  car_id: string;
+  specifications_ids: string[];
+}
+
+class CreateCarSpecificationUseCase {
+  constructor(private carsRepository: ICarsRepository) {}
+
+  async execute({ car_id, specifications_ids }: IRequest): Promise<void> {
+    const car = this.carsRepository.findById(car_id);
+    if (!car) {
+      throw new AppError('Car does not exists');
+    }
+  }
+}
+
+export { CreateCarSpecificationUseCase };
