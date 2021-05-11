8faeb5564a296c8319a46b549c817183855320fa
Natanael Lima <taelima1997@gmail.com>
2021-05-10 22:27:47 -0300

    change create in SpecificationsRepository to return created specification

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -13,6 +13,11 @@ class SpecificationsRepository implements ISpecificationsRepository {
     this.repository = getRepository(Specification);
   }
 
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specifications = await this.repository.findByIds(ids);
+    return specifications;
+  }
+
   async findByName(name: string): Promise<Specification | undefined> {
     const specification = await this.repository.findOne({
       name,
@@ -21,13 +26,18 @@ class SpecificationsRepository implements ISpecificationsRepository {
     return specification;
   }
 
-  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
-    const specification = this.repository.create({
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
+    const specification = await this.repository.create({
       name,
       description,
     });
 
     await this.repository.save(specification);
+
+    return specification;
   }
 }
 
