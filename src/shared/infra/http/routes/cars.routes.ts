4beff151ec283c655eb7e0d6c0288efbb87b6ab0
Natanael Lima <taelima1997@gmail.com>
2021-05-10 23:13:01 -0300

    add route list available

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -4,11 +4,20 @@ import { CreateCarController } from '@modules/cars/useCases/createCar/createCarC
 import { ensureAuthenticated } from '@shared/infra/middlewares/ensureAuthenticated';
 import { ensureAdmin } from '@shared/infra/middlewares/ensureAdmin';
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const listAvailableCarsController = new ListAvailableCarsController();
+
+carsRoutes.get(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  listAvailableCarsController.handle,
+);
 
 carsRoutes.post(
   '/',
