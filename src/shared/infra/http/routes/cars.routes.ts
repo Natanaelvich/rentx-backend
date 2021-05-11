7605f482a4091d34f8625c788bad8c88779ef2a6
Natanael Lima <taelima1997@gmail.com>
2021-05-10 22:39:44 -0300

    add specifications post car

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,16 +3,25 @@ import { Router } from 'express';
 import { CreateCarController } from '@modules/cars/useCases/createCar/createCarController';
 import { ensureAuthenticated } from '@shared/infra/middlewares/ensureAuthenticated';
 import { ensureAdmin } from '@shared/infra/middlewares/ensureAdmin';
+import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post(
   '/',
   ensureAuthenticated,
   ensureAdmin,
   createCarController.handle,
+);
+
+carsRoutes.post(
+  '/specifications/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarSpecificationController.handle,
 );
 
 export { carsRoutes };
