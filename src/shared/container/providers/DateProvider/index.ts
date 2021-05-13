24667805caa8a5738ea6ec6bd5066c2fe70d0efd
Natanael Lima <taelima1997@gmail.com>
2021-05-12 23:08:39 -0300

    add container provider date

---
+++ src/shared/container/providers/DateProvider/index.ts
@@ -0,0 +1,8 @@
+import { container } from 'tsyringe';
+import { IDateProvider } from './IDateProvider';
+import { DayjsDateProvider } from './implementations/DayJsDateProvider';
+
+container.registerSingleton<IDateProvider>(
+  'DayjsDateProvider',
+  DayjsDateProvider,
+);
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,3 +2,4 @@ import './StorageProvider';
 import './TemplateMailProvider';
 import './MailProvider';
 import './CacheProvider';
+import './DateProvider';
