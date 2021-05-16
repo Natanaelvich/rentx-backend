42d4103a834915af9fd4d7ca2c0622699ff13b00
Natanael Lima <taelima1997@gmail.com>
2021-05-16 08:49:21 -0300

    add variables refresh token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,9 @@
 export default {
   jwt: {
     secret: process.env.APP_SECRET || 'secret-fake',
-    exp: '1d',
+    exp: '15m',
+    secret_refresh_token: process.env.APP_SECRET_REFRESH_TOKEN || 'secret-fake',
+    expires_in_refresh_token: '30d',
+    expires_refresh_token_days: 30,
   },
 };
