28112ca903564ae20d678301960bb4d20b7354d9
Natanael Lima <taelima1997@gmail.com>
2021-05-16 08:46:31 -0300

    add AddDays

--- src/shared/container/providers/DateProvider/implementations/DayJsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayJsDateProvider.ts
@@ -27,6 +27,11 @@ class DayjsDateProvider implements IDateProvider {
       'days',
     );
   }
+
+  addDays(days: number, reference_date: Date | null = null): Date {
+    const date = reference_date ? dayjs(reference_date) : dayjs();
+    return date.add(days, 'day').toDate();
+  }
 }
 
 export { DayjsDateProvider };
