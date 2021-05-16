d59bd59b6d976a01f5ebe5b242b6327e407baaf2
Natanael Lima <taelima1997@gmail.com>
2021-05-16 09:03:16 -0300

    add /refresh-token route post

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,10 +1,13 @@
 import { AuthenticateUserController } from '@modules/accounts/useCases/authenticateUser/authenticateUserController';
+import { RefreshTokenController } from '@modules/accounts/useCases/refreshToken/RefreshTokenController';
 import { Router } from 'express';
 
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoutes.post('/', authenticateUserController.handle);
+authenticateRoutes.post('/refresh-token', refreshTokenController.handle);
 
 export { authenticateRoutes };
