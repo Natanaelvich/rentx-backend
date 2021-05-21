09201ac5430fab9b45f788ebe0b50a08a67ae582
Natanael Lima <taelima1997@gmail.com>
2021-05-21 17:19:57 -0300

    add serving coverage report

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,12 +1,13 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
+import path from 'path';
 
 import createConnection from '@shared/infra/typeorm';
 import '@shared/container';
@@ -22,6 +23,26 @@ createConnection();
 const app = express();
 app.use(cors({ credentials: true, origin: true }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(
+  '/api-coverage',
+  express.static(
+    path.resolve(__dirname, '..', '..', '..', '..', 'coverage', 'lcov-report'),
+  ),
+);
+app.get('/api-coverage', (request: Request, response: Response) => {
+  return response.sendFile(
+    path.resolve(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      '..',
+      'coverage',
+      'lcov-report',
+      'index.html',
+    ),
+  );
+});
 
 Sentry.init({
   dsn:
